eb4856c909ff96ae0732b0fe46ec78e5a0df077b
squillwah <Swagapotato@gmail.com>
2025-09-20 13:58:59 -0400

    entity system boilerplate

--- entities.c
+++ entities.c
@@ -1,13 +1,62 @@
-typedef enum components {
-    Player = 0,
-    Place,
-    Item
-} components;
 
+
+// all component types
+enum components {
+    Descriptor = 0, // Description data
+    Player,         // Player data
+    Place,          // Place data
+    Container,      // Containers data
+    Item            // Item data
+};
+
+// DESCRIPTOR
+// holds name and description of an entity
+typedef struct ComponentDescriptor {
+    char name[64];
+    char desc[512];
+} ComponentDescriptor;
+void initComponentDescriptor();
+
+// PLAYER
+// holds player data for players
 typedef struct ComponentPlayer {
     int health;
 } ComponentPlayer;
-void initComponentPlayer();
+void initComponentPlayer(ComponentPlayer* component);
+
+// PLACE
+// holds place data for places
+typedef struct ComponentPlace {
+    int temperature;
+} ComponentPlace;
+void initComponentPlace();
+
+// CONTAINER
+// holds data for entities which can contain other entities
+typedef struct ComponentContainer {
+    int num_contents;
+    int contents[64]; //@HARDCODE can contain 64 other entity IDs
+} ComponentContainer;
+void initComponentContainer();
+
+// ITEM
+// holds data for item entities
+typedef struct ComponentItem {
+    int value;
+} ComponentItem;
+void initComponentItem();
+    
+
+void initComponent(void* component, enum components type) {
+    switch (type) {
+        case Player:
+            initComponentPlayer(component);
+            break;
+        default:
+            break;
+    }
+}
+
 
 typedef enum actions {
     PickUp = 0,
